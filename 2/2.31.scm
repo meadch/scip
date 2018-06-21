5f998683f8954c54b6b8e89c0516679a545814a5
meadch <meadch@gmail.com>
2018-06-21 07:25:53 -0700

    fixes spacing

--- 2/2.31.scm
+++ 2/2.31.scm
@@ -6,7 +6,7 @@
     tree 
   ))
 
-  (define square (lambda (x) (* x x)))
-  (define (square-tree tree) (tree-map square tree))
+(define square (lambda (x) (* x x)))
+(define (square-tree tree) (tree-map square tree))
 
 (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7))) ;(1 (4 (9 16) 25) (36 49))
