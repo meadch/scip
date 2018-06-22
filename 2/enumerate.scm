546e23538e3b7586e734f0612e31722cc6372615
meadch <meadch@gmail.com>
2018-06-21 20:37:51 -0700

    add enumerate-integer

--- 2/enumerate.scm
+++ 2/enumerate.scm
@@ -1,8 +1,12 @@
 (define x (list 1 (list 2 3) 4 (list 5)))
 
+(define (enumerate-integer low high)
+  (if (> low high) `() (cons low (enumerate-integer (+ 1 low) high))))
+
 (define (enumerate-tree tree)
   (cond ((null? tree) tree)
         ((pair? tree) (append (enumerate-tree (car tree)) (enumerate-tree (cdr tree))))
         (else (list tree))))
 
 (enumerate-tree x)
+(enumerate-integer 1 5)
