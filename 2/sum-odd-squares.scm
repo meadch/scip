415a9d3e7e305e6a64e9f6806935a67e168ee631
meadch <meadch@gmail.com>
2018-06-21 20:45:30 -0700

    implements sum-odd-squares as signal processing

---
+++ 2/sum-odd-squares.scm
@@ -0,0 +1,23 @@
+(define (enumerate-tree tree)
+  (cond ((null? tree) tree)
+        ((pair? tree) (append (enumerate-tree (car tree)) (enumerate-tree (cdr tree))))
+        (else (list tree))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) `())
+        ((predicate (car sequence)) (cons (car sequence) (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate proc initial sequence)
+  (if (null? sequence) 
+    initial
+    (accumulate proc (proc initial (car sequence)) (cdr sequence))))
+
+(define a (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(define (square x) (* x x))
+
+(define (sum-odd-squares t) 
+  (accumulate + 0 (map square (filter odd? (enumerate-tree t)))))
+
+(sum-odd-squares a) ;84
