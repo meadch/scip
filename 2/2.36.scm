3ad5b1fd7a509635744cb1158ad9d0a07db41476
meadch <meadch@gmail.com>
2018-06-23 13:31:11 -0700

    removes unnecesary lambda wrapper

--- 2/2.36.scm
+++ 2/2.36.scm
@@ -12,8 +12,8 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       `()
-      (cons (accumulate op init (map (lambda (seq) (car seq)) seqs))
-            (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
 
 (define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
 (accumulate-n + 0 s) ;(22 26 30)
