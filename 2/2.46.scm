64b4d7f55a716952500ed4ccc89eee733c9b4ba6
meadch <meadch@gmail.com>
2018-07-04 08:24:15 -0700

    implements vector constructor, selectors

---
+++ 2/2.46.scm
@@ -0,0 +1,18 @@
+; Exercise 2.46. Implement a data abstraction for vectors by giving a constructor make-vect and corresponding selectors xcor-vect and ycor-vect. In terms of your selectors and constructor, implement procedures add-vect, sub-vect, and scale-vect that perform the operations vector addition, vector subtraction, and multiplying a vector by a scalar.
+
+(define make-vect cons)
+(define xcor-vect car)
+(define ycor-vect cdr)
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect scalar v)
+  (make-vect (* scalar (xcor-vect v))
+             (* scalar (ycor-vect v))))
