609a20f2e6e93c6c4f717e7478e004d1595406cf
meadch <meadch@gmail.com>
2018-07-24 19:03:52 -0700

    adds huffman utils, decodes message

---
+++ 2/2.67.scm
@@ -0,0 +1,15 @@
+
+; Exercise 2.67. Define an encoding tree and a sample message:
+
+(load "huffman-trees.scm")
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message (list 0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(decode sample-message sample-tree) ;(a d a b b c a)
